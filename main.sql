2dc044b520376aba1023df85ebc42696b3f206fd
Jahongir Jumaboyev <jjahongir2197@gmail.com>
2026-04-17 14:29:20 +0500

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,14 @@
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(50),
+    score INT
+);
+
+INSERT INTO students VALUES
+(1, 'Ali', 18, 'Tashkent', 85),
+(2, 'Vali', 19, 'Samarkand', 78),
+(3, 'Hasan', 20, 'Bukhara', 92),
+(4, 'Husan', 18, 'Tashkent', 67),
+(5, 'Olim', 21, 'Khiva', 88);
